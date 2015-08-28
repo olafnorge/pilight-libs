46d2888d6c4676e8fbb3ccfd3789329ceb910926
Volker Machon <volker@machon.biz>
2015-08-28 06:34:28 +0200

    added try catch block

--- receiver/public/index.php
+++ receiver/public/index.php
@@ -2,5 +2,9 @@
 define('DS', DIRECTORY_SEPARATOR);
 define('APP_PATH', realpath(__DIR__ . DS . '..' . DS));
 
+try {
 
+} catch (\Exception $Exception) {
+    header('HTTP/1.1 500 Internal Server Error');
+}
 
